13d446ce0d984e5837de8dc2960aef0c9384eff9
d3ntka <77463886+d3ntka@users.noreply.github.com>
2021-10-18 00:13:21 +0200

    add security

--- wp-content/themes/bechampions/functions.php
+++ wp-content/themes/bechampions/functions.php
@@ -586,3 +586,31 @@ function acf_sum_point($post_id)
 	update_field($field_name, $value, $post_id);
 }
 add_action('acf/save_post', 'acf_sum_point', 20);
+
+
+
+
+
+
+// SECURITY
+
+remove_action('wp_head', 'wp_generator');
+
+function my_secure_generator( $generator, $type ) {
+	return '';
+}
+add_filter( 'the_generator', 'my_secure_generator', 10, 2 );
+
+function my_remove_src_version( $src ) {
+	global $wp_version;
+
+	$version_str = '?ver='.$wp_version;
+	$offset = strlen( $src ) - strlen( $version_str );
+
+	if ( $offset >= 0 && strpos($src, $version_str, $offset) !== FALSE )
+		return substr( $src, 0, $offset );
+
+	return $src;
+}
+add_filter( 'script_loader_src', 'my_remove_src_version' );
+add_filter( 'style_loader_src', 'my_remove_src_version' );
